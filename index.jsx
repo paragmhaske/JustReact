56ca40e51c8aa194465a84edd970cd1f04b5c771
Parag Mhaske <parag.mhaske@otis.com>
2025-04-15 18:37:52 +0530

    Componet

--- index.jsx
+++ index.jsx
@@ -2,62 +2,44 @@ import {createRoot} from "react-dom/client";
 let root = createRoot(document.getElementById("root"));
 //root.render(<h1>Lets React</h1>);
 
-function MyAwesomeNavbar() {
+function Header() {
 return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">MyAwesomeNavbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+    <header>
+    <img src="react-logo.png" width="40px" alt="React logo" />
+</header>
 
-            <div className="collapse navbar-collapse" id="navbarsExample03">
-                <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form role="search">
-                    <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-                </form>
-            </div>
-        </div>
-    </nav>
 )
 }
-
-/**
-* Challenge:
-* Create your very first custom React component!
-* Call it "MainContent", and have it return a simple
-* h1 element that says "React is great!"
-* 
-* Afterward, render it on the line below MyAwesomeNavbar
-*/
-function MainContent() {
-return (
-    <h1>React is great!</h1>
+function Page() {
+    return (
+        <main>
+        <h1>Reason I am excited to learn React</h1>
+        <ol>
+            <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+            <li>I am more likely to get a job as a front end developer if I know React</li>
+        </ol>
+    </main>
+    
     )
     }
-root.render(
-<div>
-    <MyAwesomeNavbar />
-    <MainContent />
-    
-</div>
-)
+    function FooterCompoent() {
+        return (
+            <footer>
+        <small>© 2025 Parag development. All rights reserved.</small>
+    </footer>
+        
+        )
+        }
 
+
+function MainContent() {
+return (<>
+   <Header />
+   <MainComponent />
+   <FooterCompoent /> 
+</>
+    )
+    }
+    root.render(
+        <Page />
+    )
